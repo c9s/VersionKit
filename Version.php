dfa12db1a71bbe6077101edabbb1b2e96ad14cc1
c9s <yoanlin93@gmail.com>
2015-12-11 18:42:42 +0800

    Fix version string pattern matching

--- Version.php
+++ Version.php
@@ -59,7 +59,7 @@ class Version implements VersionProvider
      */
     protected function parseDistName($version)
     {
-        if (preg_match('/^(\w+)-(.*?)(?:-(\w+))?$/',$version, $regs)) {
+        if (preg_match('/^([a-z]\w+)-([0-9]+.*?)(?:-(\w+))?$/i',$version, $regs)) {
             return array($regs[1], $regs[2], isset($regs[3]) ? $regs[3] : null);
         }
         return null;
