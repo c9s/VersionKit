79a2dd62e11d6663cb22688e8707b157a0d7bd13
c9s <yoanlin93@gmail.com>
2015-12-11 23:02:10 +0800

    rename test case

--- tests/VersionCollectionTest.php
+++ tests/VersionCollectionTest.php
@@ -4,7 +4,7 @@ use VersionKit\VersionCollection;
 
 class VersionCollectionTest extends PHPUnit_Framework_TestCase
 {
-    public function test()
+    public function testFilter()
     {
         $versions = new VersionCollection(['php-5.3.0', 'php-5.3.3', 'php-5.4.0', 'php-5.5.0', 'php-7.0.0']);
         $versions7 = $versions->filterByMajorVersion(7);
